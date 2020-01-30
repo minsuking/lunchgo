bd50afe782dade234be5c9986db39688d0b5512c
김민수 <minsu.kim08@gmail.com>
2020-01-31 01:06:05 +0900

    Initial commit

---
+++ lunchgo-api/src/main/java/kr/co/dreamwing/lunchgo/LunchgoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.dreamwing.lunchgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchgoApplication.class, args);
+	}
+
+}
---
+++ lunchgo-api/src/test/java/kr/co/dreamwing/lunchgo/LunchgoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.dreamwing.lunchgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
